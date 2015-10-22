3ad2a59346658dbf6fbd763ffbdc3bd3340ad10f
Beer,Patrick P.J.F. de <p.debeer@fontys.nl>
2015-10-22 15:21:47 +0200

    TeacherPool classes + tests added

---
+++ src/TeacherPool.java
@@ -0,0 +1,5 @@
+/**
+ * Created by 885982 on 22-10-2015.
+ */
+public class TeacherPool {
+}
---
+++ src/TeacherPoolTest.java
@@ -0,0 +1,25 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by 885982 on 22-10-2015.
+ */
+public class TeacherPoolTest {
+
+    @org.junit.Before
+    public void setUp() throws Exception {
+
+    }
+
+    @org.junit.After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void Test()
+    {
+        assertTrue(true);
+    }
+}
